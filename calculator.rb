dd52ebfe28294b9676a7126a00ac3564066f419a
Faith Oladele <faitholadele@gmail.com>
2016-05-11 12:07:08 -0600

    psuedocode for basic calculator

--- calculator.rb
+++ calculator.rb
@@ -4,4 +4,15 @@
 # push to a github repo
 
 puts "welcome to the ruby calculator"
+
+# get first number
 puts "Enter your first number: "
+
+# get operator
+puts "Enter Operator"
+
+# Enter second number
+puts "Enter second number"
+
+# figure out how to do the math on the two numbers
+# output result to the user
